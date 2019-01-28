45c8cc70a554aa81eee1e8e5c267ae79e326f75c
André Luiz Morita <andreluizmorita@gmail.com>
2019-01-28 18:37:14 -0200

    Play List

---
+++ src/pages/browse/index.js
@@ -0,0 +1,73 @@
+import React from 'react';
+
+import { Container, Title, List, Playlist } from './styles';
+
+const Browse = () => (
+  <Container>
+    <Title>Navegar</Title>
+
+    <List>
+      <Playlist href="">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/en/3/35/FooFightersGreatestHits.jpg"
+          alt="Playlist"
+        />
+        <strong>Rock dos bons</strong>
+        <p>
+          Relaxe enquanto você programa ouvindo apenas as melhores do rock
+          mundial!
+        </p>
+      </Playlist>
+
+      <Playlist href="">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/en/3/35/FooFightersGreatestHits.jpg"
+          alt="Playlist"
+        />
+        <strong>Rock dos bons</strong>
+        <p>
+          Relaxe enquanto você programa ouvindo apenas as melhores do rock
+          mundial!
+        </p>
+      </Playlist>
+
+      <Playlist href="">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/en/3/35/FooFightersGreatestHits.jpg"
+          alt="Playlist"
+        />
+        <strong>Rock dos bons</strong>
+        <p>
+          Relaxe enquanto você programa ouvindo apenas as melhores do rock
+          mundial!
+        </p>
+      </Playlist>
+
+      <Playlist href="">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/en/3/35/FooFightersGreatestHits.jpg"
+          alt="Playlist"
+        />
+        <strong>Rock dos bons</strong>
+        <p>
+          Relaxe enquanto você programa ouvindo apenas as melhores do rock
+          mundial!
+        </p>
+      </Playlist>
+
+      <Playlist href="">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/en/3/35/FooFightersGreatestHits.jpg"
+          alt="Playlist"
+        />
+        <strong>Rock dos bons</strong>
+        <p>
+          Relaxe enquanto você programa ouvindo apenas as melhores do rock
+          mundial!
+        </p>
+      </Playlist>
+    </List>
+  </Container>
+);
+
+export default Browse;
