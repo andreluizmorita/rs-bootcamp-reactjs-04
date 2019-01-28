26b63d3a26fd3ebf793e9ddb3c46384adfadd05d
André Luiz Morita <andreluizmorita@gmail.com>
2019-01-28 19:32:48 -0200

    Redux sagas

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
 import './config/reactotron';
@@ -10,21 +11,24 @@ import Player from './components/Player';
 import Header from './components/Header';
 
 import Routes from './routes';
+import store from './store';
 
 const App = () => (
-  <BrowserRouter>
-    <Wrapper>
-      <GlobalStyle />
-      <Container>
-        <Sidebar />
-        <Content>
-          <Header />
-          <Routes />
-        </Content>
-      </Container>
-      <Player />
-    </Wrapper>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Wrapper>
+        <GlobalStyle />
+        <Container>
+          <Sidebar />
+          <Content>
+            <Header />
+            <Routes />
+          </Content>
+        </Container>
+        <Player />
+      </Wrapper>
+    </BrowserRouter>
+  </Provider>
 );
 
 export default App;
